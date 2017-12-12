d618483ad9ea55f2a63b755cfa868e0f4b4d2417
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-12-12 14:12:44 +0100

    exercice

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo-5</title>
+  </head>
+  <body>
+    <?php
+      $int;
+      settype($int, 'integer');
+      echo $int . ' ';
+      $int = 12;
+      echo $int;
+    ?>
+  </body>
+</html>
